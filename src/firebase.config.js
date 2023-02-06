78a41946647015f7adee01cd999cb9b6884a7123
Atul Kesharwani <60789960+Atu77l@users.noreply.github.com>
2023-02-07 00:14:34 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
  // addd your key and configuration details
-  apiKey: "AIzaSyCKJQe-_uG_TF-3uKeT8GG6RdFOxAWvYgM",
-    authDomain: "react-firebase-auth-38725.firebaseapp.com",
-    projectId: "react-firebase-auth-38725",
-    storageBucket: "react-firebase-auth-38725.appspot.com",
-    messagingSenderId: "432054835000",
-    appId: "1:432054835000:web:0ccd90a3ff289b073bd3cd",
-    measurementId: "G-BM0Z55B7HG"
+
 };
 
 // Initialize Firebase
